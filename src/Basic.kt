d7b89cc363d422458acfcdfb75be7ce9eed82b35
KimGiii <gibo9809@gmail.com>
2025-04-01 14:21:51 +0900

    코틀린 연습1

---
+++ src/Basic.kt
@@ -0,0 +1,25 @@
+fun parseInt(str: String): Int? {
+    return str.toIntOrNull()
+}
+
+fun printProduct(arg1: String, arg2: String) {
+    val x = parseInt(arg1)
+    val y = parseInt(arg2)
+
+    if (x == null) {
+        println("$arg1 is not a number")
+        return
+    }
+    if (y == null) {
+        println("$arg2 is not a number")
+        return
+    }
+    println(x * y)
+
+}
+
+fun main() {
+    printProduct("a", "b")
+    printProduct("8", "6")
+    printProduct("9", "c")
+}

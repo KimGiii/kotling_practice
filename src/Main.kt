b2234e4ea64c26938ec47cf4ed2772b6bb39c874
KimGiii <gibo9809@gmail.com>
2025-04-01 14:21:51 +0900

    코틀린 연습1

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,12 @@
 fun main() {
 
-//    val items = listOf("apple", "banana", "kiwi")
-//
-//    var index = 0
-//    while (index < items.size) {
-//        println("item at $index is ${items[index]}")
-//        index++
-//    }
+    val items = listOf("apple", "banana", "kiwi")
+
+    var index = 0
+    while (index < items.size) {
+        println("item at $index is ${items[index]}")
+        index++
+    }
 
     val a = readln()
     val b = readln()

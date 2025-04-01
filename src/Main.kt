1bc81d1500352d5a651483a6905d8a44db7b4138
KimGiii <gibo9809@gmail.com>
2025-04-01 12:59:35 +0900

    코틀린 연습 시작

---
+++ src/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+//    val items = listOf("apple", "banana", "kiwi")
+//
+//    var index = 0
+//    while (index < items.size) {
+//        println("item at $index is ${items[index]}")
+//        index++
+//    }
+
+    val a = readln()
+    val b = readln()
+    var max = if (a > b) a else b
+    println("max is $max")
+
+}

e42e14baf14a5f4672b5eb0c96f68f360563b4fc
KimGiii <gibo9809@gmail.com>
2025-04-02 16:02:17 +0900

    feature1 : 추가 로직

--- src/Basic2.kt
+++ src/Basic2.kt
@@ -11,6 +11,24 @@ fun getIndices(count: Int): List<Int> {
 }
 
 // 새로운 함수 필요
+fun divideOrNull(a: Int): Int {
+
+    // The try block is always executed
+    // An exception here (division by zero) causes an immediate jump to the catch block
+    try {
+        val b = 44 / a
+        println("try block: Executing division: $b")
+        return b
+    }
+
+    // The catch block is executed due to the ArithmeticException (division by zero if a ==0)
+    catch (e: ArithmeticException) {
+        println("catch block: Encountered ArithmeticException $e")
+        return -1
+    } finally {
+        println("finally block: The finally block is always executed")
+    }
+}
 
 
 fun main() {
@@ -22,4 +40,7 @@ fun main() {
     printLength(listOf(Any()))
 
     println(getIndices(-2))
+
+    // feature2
+    divideOrNull(0)
 }
